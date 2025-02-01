f41702eafc64964d512503afc17855e3ac649db3
Joe Karasha <jkarasha@gmail.com>
2025-02-01 11:40:10 -0800

    Add aks-store-demo.

--- 00-setup-bicep/setup.sh
+++ 00-setup-bicep/setup.sh
@@ -4,4 +4,8 @@ az sshkey create --name "azure-cli" --resource-group "rg-do-not-delete"
 # Create an SSH key pair using ssh-keygen
 ssh-keygen -t rsa -b 4096
 
+# Create the cluster
 az deployment group create --resource-group rg-aks-basics-bicep --template-file main.bicep --parameters dnsPrefix=<dns-prefix> linuxAdminUsername=<linux-admin-username> sshRSAPublicKey='<ssh-key>'
+
+# connect to the cluster
+az aks get-credentials --resource-group rg-aks-basics-bicep --name aks-a36800
